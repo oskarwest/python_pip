a34931ac4f57b5b691b10a329832c1f698345ddb
oskarwest <oskarwest96@gmail.com>
2023-05-30 10:20:34 -0500

    solicitudes con request y uso de pandas para el manejo del csv

--- app/main.py
+++ app/main.py
@@ -1,24 +1,40 @@
 import utils
 import read_csv
 import charts
+import pandas as pd
 
 
 def run():
+
+
+  ''''
   data = read_csv.read_csv('data.csv')
   data = list(filter(lambda item: item['Continent'] == 'South America', data))
 
   countries = list(map(lambda x: x['Country/Territory'], data))
   percentages = list(map(lambda x: x['World Population Percentage'], data))
   charts.generate_pie_chart(countries, percentages)
+  ''' 
 
+  df = pd.read_csv('data.csv') #leer el dataset
+  df= df[df['Continent'] == 'Africa'] # asi se filtra toda la informacion de los paises del continente:
+
+  countries=df['Country/Territory'].values  #seleccionamos la columna con el nombre del pais
+  percentages=df['World Population Percentage'].values   #seleccionamos la columna con el porcentaje de poblacion
+  charts.generate_pie_chart(countries, percentages)  #generamos un grafico con los paises y su porcentaje de poblacion
+
+
+  data = read_csv.read_csv('data.csv')
   country = input('Type Country => ')
 
   result = utils.population_by_country(data, country)
-
+  print(result)
+  
   if len(result) > 0:
     country = result[0]
     labels, values = utils.get_population(country)
     charts.generate_bar_chart(country['Country/Territory'], labels, values)
+
 
 
 if __name__ == '__main__':
